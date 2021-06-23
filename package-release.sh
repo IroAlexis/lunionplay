8f7682b1ee653143ea92d026e8f9a2c81a90b6a6
IroAlexis <iroalexis@outlook.fr>
2021-06-23 16:00:04 +0200

    [build] Script for build a standalone package

---
+++ package-release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ "$DEBUG" = 1 ]; then
+  set -x
+fi
+
+pkgname=lunion-play
+pkgver=$(git describe --tags)
+_where="$(dirname "$(which "$0")")"
+
+
+mkdir -p /tmp/$pkgname-$pkgver
+
+install -Dm755 lunion-play /tmp/$pkgname-$pkgver
+install -Dm755 lunion-gamesetup /tmp/$pkgname-$pkgver
+install -Dm644 customization.cfg /tmp/$pkgname-$pkgver
+install -Dm444 LICENSE /tmp/$pkgname-$pkgver
+
+cd /tmp
+tar --zstd -cf $pkgname-$pkgver.tar.zst $pkgname-$pkgver
+mv $pkgname-$pkgver.tar.zst $_where
+
+rm -rf /tmp/$pkgname-$pkgver
+
+exit 0

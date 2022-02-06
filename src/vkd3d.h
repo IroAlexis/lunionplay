ee4d6867e3d74d40c1f7fd44528500b1608d5ff8
IroAlexis <iroalexis@outlook.fr>
2022-02-06 15:09:07 +0100

    lunionplay: Implement vkd3d module

---
+++ src/vkd3d.h
@@ -0,0 +1,27 @@
+/*
+ * vkd3d.h
+ * Copyright (C) 2022 IroAlexis <iroalexis@outlook.fr>
+ *
+ * lunionplay is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lunionplay is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef __VKD3D__
+#define __VKD3D__
+
+
+void lunionplay_set_vkd3d_proton_env(void);
+
+
+#endif
